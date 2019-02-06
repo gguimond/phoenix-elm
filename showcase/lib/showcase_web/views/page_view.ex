d0389d793341b24af687e0b42116223fb21b56d6
Guillaume Guimond <guillaume.guimond@orange.com>
2019-02-06 17:05:40 +0100

    playing with templates

--- showcase/lib/showcase_web/views/page_view.ex
+++ showcase/lib/showcase_web/views/page_view.ex
@@ -8,4 +8,15 @@ defmodule ShowcaseWeb.PageView do
   def render("page.json", %{page: page}) do
     %{title: page.title}
   end
+
+  def handler_info(conn) do
+    "Request Handled By: #{controller_module(conn)}.#{action_name(conn)}"
+  end
+
+  def connection_keys(conn) do
+    conn
+    |> Map.from_struct()
+    |> Map.keys()
+  end
+  
 end
---
+++ showcase/lib/showcase_web/views/shared_view.ex
@@ -0,0 +1,3 @@
+defmodule ShowcaseWeb.SharedView do
+  use ShowcaseWeb, :view
+end
